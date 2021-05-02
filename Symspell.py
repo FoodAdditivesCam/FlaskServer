6550b7602c5e240a135adea220772bd5b09c5701
beansbin <beans_bin@naver.com>
2021-05-02 12:52:43 +0900

    Add Symsepll function

---
+++ Symspell.py
@@ -0,0 +1,30 @@
+# Symspell 결과 반환
+from flask import request
+from flask_restx import Resource, Api, Namespace
+from symspellpy import SymSpell, Verbosity
+import json
+
+# 이 페이지의 이름이 Symspell이라고 정의해주는 것.
+Symspell = Namespace('Symspell')
+
+sym_spell = SymSpell()
+dictionary_path = 'dictionary.txt'
+sym_spell.load_dictionary(dictionary_path, 0, 1, separator="$")
+
+# 3.35.255.25/Symsepll/<input_term> 주소가 들어왔을 때 실행
+@Symspell.route('/<string:input_term>')
+class TodoPost(Resource):
+    def get(self, input_term):
+        # input_term 오타 교정
+        suggestions = sym_spell.lookup(
+            input_term, Verbosity.CLOSEST,
+            max_edit_distance=2, include_unknown=True)  # include_unknown : 일치하는 결과가 없을 경우 입력 단어 반환
+
+        result = {}
+        count = 0
+        for suggestion in suggestions:
+            result[count] = str(suggestion).split(',')[0]
+            count += 1
+
+        # json 형식으로 반환
+        return json.dumps(result)
--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+# 서버가 돌아가는 메인 페이지
 from flask import Flask
 from flask_restx import Resource, Api
+from Symspell import Symspell
+
 
 app = Flask(__name__)
 api = Api(app)
 
+# 분리한 파일을 api에 등록
+api.add_namespace(Symspell, '/symspell')
 
-@api.route('/hello/<string:name>')
-class Hello(Resource):
-    def get(self, name):
-        return {"message": "Welcome, %s!" % name}
-
-
+# 서버 실행
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
