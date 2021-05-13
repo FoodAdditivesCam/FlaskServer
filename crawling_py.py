4fe5c212184f9e5d07e35c296c279fc9cfa2c8a3
Minjeong Kim <alswjd980512@naver.com>
2021-05-13 19:31:22 +0900

    crawling python 코드, not flask

--- crawling_py.py
+++ crawling_py.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
 from selenium import webdriver
-
+# todo: bs4, selenium install
 
 def crawling(keyword):
     baseUrl = 'https://www.google.com/search?q='
     url = baseUrl + quote_plus(keyword)
 
-    # webdriver option 설정
+    # todo: webdriver option 설정
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
     chrome_options.add_argument("--no-sandbox")  # GUI를 사용할 수 없는 환경에서 설정, linux, docker 등
@@ -16,13 +16,14 @@ def crawling(keyword):
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
 
-    # 읽어온 페이지의 html 코드 받아오기
+    # todo: 읽어온 페이지의 html 코드 받아오기
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
 
     result = {}  # 검색 결과의 리스트 저장, key=문서이름, value=문서링크
     lists = soup.select('#rso > .g')
-    # 검색 결과의 리스트 저장
+
+    # todo: 검색 결과의 리스트 저장
     for list in lists:
         name = list.select_one('.LC20lb.DKV0Md').text
         print(name)
@@ -33,7 +34,7 @@ def crawling(keyword):
         result[name] = link
 
     contents = {}  # 모든 문서의 내용 저장, key=문서이름, value=문서 body 내용
-    # 각 문서에 대한 html body부분 저장
+    # todo: 각 문서에 대한 html body부분 저장
     for key, val in result.items():
         print(val)
         driver.get(val)
@@ -47,7 +48,7 @@ def crawling(keyword):
     return contents
 
 # main code
-keywords = ['토마토', '코코아']
+keywords = ['토마토', '코코아'] # 검색어 리스트
 result={}
 for i in keywords:
     result[i] = crawling(i)
