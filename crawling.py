9fe0f9fa7c00030d419cdf35cfa85186d74ee75d
Minjeong Kim <alswjd980512@naver.com>
2021-05-17 14:49:01 +0900

    combine crawling with token

--- crawling.py
+++ crawling.py
@@ -7,6 +7,19 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
 from selenium import webdriver
+# -*- coding: cp949 -*-
+from krwordrank.sentence import summarize_with_sentences
+from krwordrank.word import KRWordRank
+from newspaper import Article
+from konlpy.tag import Kkma
+from konlpy.tag import Twitter
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import normalize
+import numpy as np
+# todo: install krwordrank
+
+from data_list import get_stopwords
 
 # 이 페이지의 이름이 Crawling이라고 정의해주는 것.
 Crawling = Namespace('Crawling')
@@ -15,35 +28,83 @@ Crawling = Namespace('Crawling')
 @Crawling.route('/<string:keyword>/<int:size>')
 class Search(Resource):
     def get(self, keyword, size):
-        keywords=[]  # 검색어 리스트 toy
+        # crawling
+        keywords=[]  # 검색어 리스트
         keywords.append(keyword)
         search_filtering = ' -유튜브, -아프리카티비, -지마켓, -옥션, -쇼핑몰, -TV'
         search_words = ['', ' 사전', ' 효능', ' 부작용']  # 검색어 option list
-        result = {}
+        result = []
         for keyword in keywords:
             for search_word in search_words:
                 final_search_word = keyword + search_word+search_filtering
-                result[final_search_word] = self.crawling(final_search_word, size)
-                # resp = self.crawling(keyword + search_word, size)
+                result+=self.crawling(final_search_word, size)
+
+        # tokenize
+        url = result # url list
+        sentences = []
+        sent_tokenize = SentenceTokenizer()
+        for i in range(0, len(url)):
+            sentences += (sent_tokenize.url2sentences(url[i]))
+        print(sentences)
+        nouns = sent_tokenize.get_nouns(sentences)
+        print(nouns)
+
+        # train KR-WordRank model
+        wordrank_extractor = KRWordRank(
+            min_count=5,  # 단어의 최소 출현 빈도수 (그래프 생성 시)
+            max_length=10,  # 단어의 최대 길이
+            verbose=True
+        )
+
+        beta = 0.85  # PageRank의 decaying factor beta
+        max_iter = 10
+        keywords, rank, graph = wordrank_extractor.extract(sentences, beta, max_iter)
+        stopwords = get_stopwords()
+
+        # Check top 30 keywords with corresponding score
+        for word, r in sorted(keywords.items(), key=lambda x: -x[1])[:30]:
+            if not (word in stopwords):
+                print('%8s : %.4f' % (word, r))
+
+        # 핵심 문장 추출하기
+        # remove stopwords
+        passwords = {word: score for word, score in sorted(
+            keywords.items(), key=lambda x: -x[1])[:300] if not (word in stopwords)}
+
+        print('num passwords = {}'.format(len(passwords)))
+
+        penalty = lambda x: 0 if (25 <= len(x) <= 80 and not '마지막' in x) else 1,
+        keywords, sents = summarize_with_sentences(
+            sentences,
+            penalty=penalty,
+            stopwords=stopwords,
+            diversity=0.5,
+            num_keywords=100,
+            num_keysents=10,
+            verbose=False
+        )
+
+        for i in sents:
+            print(i)
 
         # 결과 json으로 출력
         message = {
             'status': 200,
             'message': 'OK',
-            'content': result
+            'content': sents
         }
         resp = jsonify(message)
         resp.status_code = 200
-        # print(resp)
         return resp
 
+    # crawling method: output is link list
     def crawling(self, keyword, size):
         baseUrl = 'https://www.google.com/search?q='
         url = baseUrl + quote_plus(keyword)
 
 
         # todo: webdriver option 설정
-        path = '/home/ubuntu/FlaskServer/chromedriver' # chromedriver.exe /home/ubuntu/FlaskServer/chromedriver
+        path = 'chromedriver.exe' # chromedriver.exe /home/ubuntu/FlaskServer/chromedriver
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('headless')
         chrome_options.add_argument("--no-sandbox")  # GUI를 사용할 수 없는 환경에서 설정, linux, docker 등
@@ -57,7 +118,7 @@ class Search(Resource):
         soup = BeautifulSoup(html, "lxml")
 
         link_result = {}  # 검색 결과의 리스트 저장, key=문서이름, value=문서링크
-        link_result_2 = []
+        link_result_2 = [] # 리스트만
         lists = soup.select('#rso > .g')
 
         # 가져올 사이트 개수 조정
@@ -75,126 +136,42 @@ class Search(Resource):
                 break
         print(link_result_2)
 
-        # contents = {}  # 모든 문서의 내용 저장, key=문서이름, value=문서 body 내용
-        # # todo: 각 문서에 대한 html body부분 저장
-        # for key, val in link_result.items():
-        #     driver.get(val)
-        #
-        #     html = driver.page_source
-        #     soup = BeautifulSoup(html, "lxml")
-        #
-        #     # html의 태그 추출해서 삭제
-        #     [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]  # header, footer
-        #     content = soup.find('body').getText()
-        #
-        #     content = content.replace("\n", " ")
-        #     contents[key] = content
-        #     print(key)
-        # driver.close()
-
-        # json 형식으로 반환
-        return link_result
+        return link_result_2
 
 
+class SentenceTokenizer(object):
+    def __init__(self):
+        self.kkma = Kkma()
+        self.twitter = Twitter()
+        self.stopwords = ['중인' ,'만큼', '마찬가지', '꼬집었', "연합뉴스", "데일리", "동아일보", "중앙일보", "조선일보", "기자"
+        ,"아", "휴", "아이구", "아이쿠", "아이고", "어", "나", "우리", "저희", "따라", "의해", "을", "를", "에", "의", "가",]
+    def url2sentences(self, url):
+        article = Article(url, language='ko')
+        article.download()
+        article.parse()
+        sentences = self.kkma.sentences(article.text)
 
+        for idx in range(0, len(sentences)):
+            if len(sentences[idx]) <= 10:
+                sentences[idx-1] += (' ' + sentences[idx])
+                sentences[idx] = ''
+        return sentences
 
+    def text2sentences(self, text):
+        sentences = self.kkma.sentences(text)
+        for idx in range(0, len(sentences)):
+            if len(sentences[idx]) <= 10:
+                sentences[idx-1] += (' ' + sentences[idx])
+                sentences[idx] = ''
+        return sentences
 
-# 셀레니움 없이
-    # url = "https://www.google.com/search?q={}".format(quote_plus(keyword))
-    # print(url)
-    # headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-    # res = requests.get(url,headers)
-    # soup = BeautifulSoup(res.text, 'html.parser')  # lxml
-    # print(soup)
-    # find = soup.find_all('.kCrYT')
-    # r=soup.select('.r')
-    # for i in r:
-    #     print(i.a.attrs['href'])
-    # find = soup.find('span', class_='BNeawe')
-
-    # number = soup.select('.kCrYT')
-    # names = []
-    # links = []
-    # for i in number:
-    #     try:
-    #         name = i.select_one('.BNeawe.vvjwJb.AP7Wnd').text
-    #         names.append(name)
-    #         link = i.a.attrs['href']
-    #         print(link)
-    #         link = link.lstrip('/url?q=')
-    #         link = link.split('&')
-    #         links.append(link[0])
-    #     except:
-    #         pass
-    # print("------")
-    # print(names)
-    # print(links)
-    # exit()
-    # # print(find)
-    # print(number)
-    # exit()
-    # number = number[number.find('약', ) + 2:number.rfind('개')]
-    # number = int(number.replace(',', ''))
-    # return {'keyword': keyword, 'number': number}
-
-    # tF2Cxc, yuRUbf, & 전 uri
-    # href="/url?q= , kCrYT or BNeawe
+    def get_nouns(self, sentences):
+        nouns = []
+        for sentence in sentences:
+            if sentence is not '': # SyntaxWarning: "is not" with a literal. Did you mean "!="?
+                nouns.append(' '.join([noun for noun in self.twitter.nouns(str(sentence))
+            if noun not in self.stopwords and len(noun) > 1]))
+        return nouns
 
 
 
-# # 셀레니움 있음
-#     def get(self, keyword):
-#         baseUrl = 'https://www.google.com/search?q='
-#         url = baseUrl + quote_plus(keyword)
-#
-#         # webdriver option 설정
-#         chrome_options = webdriver.ChromeOptions()
-#         chrome_options.add_argument('headless')
-#         chrome_options.add_argument("--no-sandbox")  # GUI를 사용할 수 없는 환경에서 설정, linux, docker 등
-#         chrome_options.add_argument("--disable-gpu")  # GUI를 사용할 수 없는 환경에서 설정, linux, docker 등
-#         chrome_options.add_argument('lang=ko_KR')
-#         driver = webdriver.Chrome(options=chrome_options)
-#         driver.get(url)
-#
-#         # 읽어온 페이지의 html 코드 받아오기
-#         html = driver.page_source
-#         soup = BeautifulSoup(html, "lxml")
-#
-#         result = {} # 검색 결과의 리스트 저장, key=문서이름, value=문서링크
-#         lists = soup.select('#rso > .g')
-#         # 검색 결과의 리스트 저장
-#         for list in lists:
-#             name = list.select_one('.LC20lb.DKV0Md').text
-#             print(name)
-#
-#             link = list.a.attrs['href']
-#             print(link)
-#             print()
-#             result[name] = link
-#
-#         contents= {} # 모든 문서의 내용 저장, key=문서이름, value=문서 body 내용
-#         # 각 문서에 대한 html body부분 저장
-#         for key, val in result.items():
-#             print(val)
-#             driver.get(val)
-#             html = driver.page_source
-#             soup = BeautifulSoup(html, "lxml")
-#             content = soup.find('body').text
-#             content = content.replace("\n", " ")
-#             contents[key]=content
-#             print(content)
-#         driver.close()
-#
-#         # 결과 json으로 출력
-#         message = {
-#             'status': 200,
-#             'message': 'OK',
-#             'scores': result,
-#             'content': contents
-#         }
-#         resp = jsonify(message)
-#         resp.status_code = 200
-#         print(resp)
-#
-#         # json 형식으로 반환
-#         return resp
