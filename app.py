1ecf630baac8aedccc7b40810bb129172040ed6f
Minjeong Kim <alswjd980512@naver.com>
2021-06-08 01:15:36 +0900

    add db

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from flask import Flask, request, jsonify
 from Symspell import Symspell, sym_spell
 from flask import request, jsonify
 import json
+import os
+from models import data_update, db, get_db_data
 
 
 app = Flask(__name__)
@@ -26,6 +28,27 @@ app.config['JSON_AS_ASCII'] = False
 
 dictionary_path = 'dictionary.txt'
 sym_spell.load_dictionary(dictionary_path, 0, 1, separator="$")
+
+# DB
+basdir = os.path.abspath(os.path.dirname(__file__))
+# basdir °æ·Î¾È¿¡ DBÆÄÀÏ ¸¸µé±â
+dbfile = os.path.join(basdir, 'db.sqlite') # D:‚2021‚GradProject‚FlaskServer‚db.sqlite
+# SQLAlchemy ¼³Á¤
+# ³»°¡ »ç¿ë ÇÒ DB URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+# ºñÁö´Ï½º ·ÎÁ÷ÀÌ ³¡³¯¶§ Commit ½ÇÇà(DB¹Ý¿µ)
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# ¼öÁ¤»çÇ×¿¡ ´ëÇÑ TRACK
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# SECRET_KEY
+app.config['SECRET_KEY'] = 'd42dc573f4bf3f3f77639b715d6266f1'
+db.init_app(app)
+db.app = app
+db.create_all()
+
+# data update
+#data_update()
+
 
 @app.route('/result', methods=['POST'])
 def post():
@@ -41,44 +64,49 @@ def post():
     result = symspell(jsonArray)
     print(result)
 
-    # url ¸®½ºÆ® ¹Þ¾Æ¿À±â
-    count = 2
-    url_list = getURL(result, count)
-    print("oh")
-    print(url_list)
+    # # url ¸®½ºÆ® ¹Þ¾Æ¿À±â
+    # count = 2
+    # url_list = getURL(result, count)
+    # print("oh")
+    # print(url_list)
+    #
+    # # Å°¿öµå¿Í ¼³¸í ¹Þ¾Æ¿À±â
+    # jsonDic = {}
+    # for i in range(0, len(url_list)):
+    #     # ÀÏ´Ü ¿À·ù ¸µÅ© Á¦¿Ü
+    #     try:
+    #         print(url_list[i])
+    #         article = Article(url_list[i], language='ko')
+    #         article.download()
+    #         article.parse()
+    #         kkma = Kkma()
+    #         sentences = kkma.sentences(article.text)
+    #         for idx in range(0, len(sentences)):
+    #             if len(sentences[idx]) <= 10:
+    #                 sentences[idx - 1] += (' ' + sentences[idx])
+    #                 sentences[idx] = ''
+    #         if len(sentences) < 10:
+    #             continue
+    #     except:
+    #         continue
+    #     dic = {}
+    #     keyword, word, sent = getResult(url_list[i], result[i % count])
+    #     dic["word"] = word
+    #     dic["sent"] = sent
+    #     jsonDic[keyword] = dic
+    #
+    # jsonObject = json.dumps(jsonDic, ensure_ascii=False)
+    # print(jsonObject)
 
-    # Å°¿öµå¿Í ¼³¸í ¹Þ¾Æ¿À±â
-    jsonDic = {}
-    for i in range(0, len(url_list)):
-        # ÀÏ´Ü ¿À·ù ¸µÅ© Á¦¿Ü
-        try:
-            print(url_list[i])
-            article = Article(url_list[i], language='ko')
-            article.download()
-            article.parse()
-            kkma = Kkma()
-            sentences = kkma.sentences(article.text)
-            for idx in range(0, len(sentences)):
-                if len(sentences[idx]) <= 10:
-                    sentences[idx - 1] += (' ' + sentences[idx])
-                    sentences[idx] = ''
-            if len(sentences) < 10:
-                continue
-        except:
-            continue
-        dic = {}
-        keyword, word, sent = getResult(url_list[i], result[i % count])
-        dic["word"] = word
-        dic["sent"] = sent
-        jsonDic[keyword] = dic
-
-    jsonObject = json.dumps(jsonDic, ensure_ascii=False)
-    print(jsonObject)
+    # get data from db
+    jsonObject = get_db_data(result)
+    jsonResp = json.dumps(jsonObject, ensure_ascii=False)
+    print(jsonResp)
 
     message = {
         'status': 200,
         'message': 'OK',
-        'scores': jsonObject
+        'scores': jsonResp
     }
     resp = jsonify(message)
     resp.status_code = 200
---
+++ models.py
@@ -0,0 +1,69 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Text, String, DateTime
+
+db = SQLAlchemy()
+engine = create_engine('sqlite:///D:/2021/GradProject/FlaskServer/db.sqlite', echo=False)
+table_name = 'Raw_material_info'
+
+class Raw_material_info(db.Model):
+    __tablename__ = 'Raw_material_info'
+
+    id = db.Column(db.Integer, primary_key=True)#db.Column(db.String(256), primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    description = db.Column(db.Text(), default="")
+    tag1 = db.Column(db.String(256), default="")
+    tag2 = db.Column(db.String(256), default="")
+    tag3 = db.Column(db.String(256), default="")
+    tag4 = db.Column(db.String(256), default="")
+    tag5 = db.Column(db.String(256), default="")
+    reference = db.Column(db.String(256), default="")
+    link = db.Column(db.Text(), default="")
+
+    def __init__(self, id, name,description, tag1,tag2,tag3,tag4,tag5,reference,link):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.tag1 = tag1
+        self.tag2 = tag2
+        self.tag3 = tag3
+        self.tag4 = tag4
+        self.tag5 = tag5
+        self.reference = reference
+        self.link = link
+
+def data_update():
+    data = pd.read_csv(r'D:\2021\GradProject\Data\db1.csv', encoding='cp949')
+    data.to_sql(
+        'Raw_material_info',
+        engine,
+        if_exists='replace',
+        index=False,
+        chunksize=500,
+        dtype={
+            "id": Integer, #db.Column(db.String(256), primary_key=True),
+            "name": String(256),
+            "description": Text,
+            "tag1": String(256),
+            "tag2": String(256),
+            "tag3": String(256),
+            "tag4": String(256),
+            "tag5": String(256),
+            "reference": String(256),
+            "link": Text,
+        }
+    )
+
+def get_db_data(input):
+    final_df=pd.DataFrame(columns=['id','name','description', 'tag1', 'tag2', 'tag3', 'tag4','tag5', 'reference', 'link'])
+    for i in input:
+        sql_df = pd.read_sql(
+            "SELECT * FROM Raw_material_info WHERE name='"+i+"'",
+            con=engine
+        )
+        final_df = pd.concat([final_df,sql_df])
+    final_dic = final_df.to_dict('records')
+    return final_dic
