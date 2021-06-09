8a02d3c42de43ea1569b9d4fa3f2d4754a089fcb
beansbin <beans_bin@naver.com>
2021-06-09 15:04:29 +0900

    워드클라우드 수정

--- app.py
+++ app.py
@@ -133,8 +133,7 @@ def post():
     plt.imshow(wordcloud, interpolation='lanczos')  # 이미지의 부드럽기 정도
     plt.axis('off')  # x y 축 숫자 제거
     plt.show()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    plt.savefig(BASE_DIR+'\\static\\picture.png', dpi=300)
+    plt.savefig('./static/picture.png', dpi=300)
 
     message = {
         'status': 200,
